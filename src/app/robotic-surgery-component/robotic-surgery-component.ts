edfa7fd453c94e7dab814f34e9e7caf251d2c8b9
Mateo Parra <mateo.parra@tory-tech.com>
2026-01-28 16:53:45 -0500

    robotic part almost ready

--- src/app/robotic-surgery-component/robotic-surgery-component.ts
+++ src/app/robotic-surgery-component/robotic-surgery-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FooterClau } from '../footer-clau/footer-clau';
 
 @Component({
   selector: 'app-robotic-surgery-component',
-  imports: [],
+  imports: [FooterClau],
   templateUrl: './robotic-surgery-component.html',
   styleUrl: './robotic-surgery-component.css'
 })
